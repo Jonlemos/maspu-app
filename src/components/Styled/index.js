dd95fbca4bac66f3e1b35b72f02d3833e70290cb
jonlemos <lemos.anjos@gmail.com>
2019-08-01 20:04:40 -0300

    Initial commit

---
+++ src/components/Styled/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  box-sizing: border-box;
+  margin: 5% 15%;
+  height:460px;
+  background-color: #000;
+`;
+
+export { 
+    Container 
+}
---
+++ src/layouts/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <h1 style={{color: this.props.color, margin: this.props.margin}}>Login</h1>
+        )
+    }
+    
+}
