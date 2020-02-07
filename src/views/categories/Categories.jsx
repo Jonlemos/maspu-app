fb9601b376bceccf7ca992aa6ee924a012ac1ed0
jonlemos <lemos.anjos@gmail.com>
2020-02-07 15:49:46 -0300

    adicionado map na requisição

--- src/views/categories/Categories.jsx
+++ src/views/categories/Categories.jsx
@@ -3,6 +3,73 @@ import { Container, Card, Button, Accordion } from 'react-bootstrap';
 import './Categories.css';
 
 export default class Categories extends React.Component {
+    constructor(props) {
+        super(props);
+        this.categories = [
+            {
+                productName: 'Coca-Cola',
+                description: '500ml',
+                price: 5,
+                category: 1,
+                nameCategory: 'Bebidas',
+                imagem:
+                    'https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg',
+            },
+            {
+                productName: 'Heineken',
+                description: '450ml',
+                price: 5,
+                category: 1,
+                nameCategory: 'Bebidas',
+                imagem:
+                    'https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg',
+            },
+            {
+                productName: 'Chá',
+                description: '330ml',
+                price: 5,
+                category: 1,
+                nameCategory: 'Bebidas',
+                imagem:
+                    'https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg',
+            },
+
+            {
+                productName: 'Bombom',
+                description: '1un',
+                price: 5,
+                category: 2,
+                nameCategory: 'Doces',
+                imagem:
+                    'https://basmar.com.br/image/cache/data/PRODUTOS%20NOVOS/Sonho-de-Valsa-13-800x800.jpg',
+            },
+            {
+                productName: 'Fandangos',
+                description: '450ml',
+                price: 5,
+                category: 2,
+                nameCategory: 'Doces',
+                imagem:
+                    'https://basmar.com.br/image/cache/data/PRODUTOS%20NOVOS/Sonho-de-Valsa-13-800x800.jpg',
+            },
+            {
+                productName: 'Paçoca',
+                description: '330ml',
+                price: 5,
+                category: 2,
+                nameCategory: 'Doces',
+                imagem:
+                    'https://basmar.com.br/image/cache/data/PRODUTOS%20NOVOS/Sonho-de-Valsa-13-800x800.jpg',
+            },
+        ];
+    }
+
+    products = () => {
+        axios.get('/produts').then(response => {
+            console.log(response);
+        });
+    };
+
     render() {
         return (
             <Container className="container-product">
@@ -12,66 +79,29 @@ export default class Categories extends React.Component {
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                         <section className="section-card">
-                            <Card
-                                className="Card-See"
-                                style={{ width: '18rem' }}
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
-                                />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">
-                                        Go somewhere
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                            <Card
-                                className="Card-See"
-                                style={{ width: '18rem' }}
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
-                                />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">
-                                        Go somewhere
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                            <Card
-                                className="Card-See"
-                                style={{ width: '18rem' }}
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
-                                />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">
-                                        Go somewhere
-                                    </Button>
-                                </Card.Body>
-                            </Card>
+                            {this.categories.map((categories, i) => (
+                                <Card
+                                    key={i}
+                                    className="Card-See"
+                                    style={{ width: '18rem' }}
+                                >
+                                    <Card.Img
+                                        variant="top"
+                                        src={categories.imagem}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>
+                                            {this.props.productName}
+                                        </Card.Title>
+                                        <Card.Text>
+                                            {categories.price}
+                                        </Card.Text>
+                                        <Button variant="primary">
+                                            Adicionar
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            ))}
                         </section>
                     </Accordion.Collapse>
                 </Accordion>
