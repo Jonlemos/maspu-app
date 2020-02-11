d3f8ce4c5d137daf0b316f09df284e85b5a1101e
jonlemos <lemos.anjos@gmail.com>
2020-02-11 19:43:35 -0300

    Criado estados do componente buttons valor

--- src/views/categories/Categories.jsx
+++ src/views/categories/Categories.jsx
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { Container, Card, Button, Accordion, Row } from 'react-bootstrap';
+import Buttons from './Buttons';
+import FloatingBar from './FloatingBar';
 import './Categories.css';
 import axios from 'axios';
 
 export default class Categories extends Component {
-
-    state = {
-        products: [],
-        counter: 0
-    };
-
-    increment(a) {
-        this.setState({ counter: a === a ? this.state.counter + 1 : this.state.counter })
-    }
-
-    decrement(a) {
-        this.setState({ counter: a === a ? this.state.counter - 1 : this.state.counter })
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+            selectedProducts: [],
+        };
     }
 
     componentDidMount() {
@@ -28,119 +24,135 @@ export default class Categories extends Component {
         });
     }
 
+    selectProduct = product => {
+        this.setState.selectedProducts = product;
+        console.log(this.state);
+    };
+
     render() {
         return (
-            <Container className="container-product">
-                <Accordion defaultActiveKey="0">
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <h1>Bebidas</h1>
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="0">
-                        <section className="section-card">
-                            {this.state.products.map((categories, i) => (
+            <div style={{ position: 'relative' }}>
+                <FloatingBar />
+                <Container className="container-product">
+                    <Accordion defaultActiveKey="0">
+                        <Accordion.Toggle
+                            as={Button}
+                            className="align-toggle"
+                            variant="link"
+                            eventKey="0"
+                        >
+                            <h1>Bebidas</h1>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <section className="section-card">
+                                {this.state.products.map((product, i) => (
+                                    <div key={i}>
+                                        <Card
+                                            className="Card-See"
+                                            style={{ width: '18rem' }}
+                                        >
+                                            <Card.Img
+                                                variant="top"
+                                                src={product.image}
+                                            />
+                                            <Card.Body>
+                                                <Card.Title>
+                                                    {product.title}
+                                                </Card.Title>
+                                                <Card.Text>
+                                                    R$ {product.price}
+                                                </Card.Text>
+                                                <Buttons
+                                                    selectProduct={quantity =>
+                                                        this.selectProduct({
+                                                            quantity,
+                                                            product,
+                                                        })
+                                                    }
+                                                />
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                ))}
+                            </section>
+                        </Accordion.Collapse>
+                    </Accordion>
+                    {/* <Accordion defaultActiveKey="1">
+                        <Accordion.Toggle
+                            as={Button}
+                            className=""
+                            variant="link"
+                            eventKey="1"
+                        >
+                            <h1>Bombonie</h1>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <section className="section-card">
                                 <Card
-                                    key={i}
                                     className="Card-See"
                                     style={{ width: '18rem' }}
                                 >
                                     <Card.Img
                                         variant="top"
-                                        src={categories.image}
+                                        src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
                                     />
                                     <Card.Body>
-                                        <Card.Title>
-                                            {categories.title}
-                                        </Card.Title>
+                                        <Card.Title>Card Title</Card.Title>
                                         <Card.Text>
-                                            R$ {categories.price}
+                                            Some quick example text to build on
+                                            the card title and make up the bulk
+                                            of the card's content.
                                         </Card.Text>
-                                        <Row className="button-aling">
-                                            <Button key={i} onClick={() => this.decrement(i)} variant="primary">
-                                                -
-                                            </Button>
-                                            <div> {this.state.counter} </div>
-                                            <Button key={i} onClick={() => this.increment(i)} variant="primary">
-                                                +
-                                            </Button>
-
-                                        </Row>
-
+                                        <Button variant="primary">
+                                            Go somewhere
+                                        </Button>
                                     </Card.Body>
                                 </Card>
-                            ))}
-                        </section>
-                    </Accordion.Collapse>
-                </Accordion>
-                <Accordion defaultActiveKey="1">
-                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                        <h1>Bombonie</h1>
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="1">
-                        <section className="section-card">
-                            <Card
-                                className="Card-See"
-                                style={{ width: '18rem' }}
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
-                                />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">
-                                        Go somewhere
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                            <Card
-                                className="Card-See"
-                                style={{ width: '18rem' }}
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
-                                />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">
-                                        Go somewhere
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                            <Card
-                                className="Card-See"
-                                style={{ width: '18rem' }}
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
-                                />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the
-                                        card title and make up the bulk of the
-                                        card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">
-                                        Go somewhere
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                        </section>
-                    </Accordion.Collapse>
-                </Accordion>
-            </Container>
+                                <Card
+                                    className="Card-See"
+                                    style={{ width: '18rem' }}
+                                >
+                                    <Card.Img
+                                        variant="top"
+                                        src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on
+                                            the card title and make up the bulk
+                                            of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">
+                                            Go somewhere
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                                <Card
+                                    className="Card-See"
+                                    style={{ width: '18rem' }}
+                                >
+                                    <Card.Img
+                                        variant="top"
+                                        src="https://static.carrefour.com.br/medias/sys_master/images/images/hd7/hd6/h00/h00/12175683682334.jpg"
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>Card Title</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on
+                                            the card title and make up the bulk
+                                            of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">
+                                            Go somewhere
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </section>
+                        </Accordion.Collapse>
+                    </Accordion> */}
+                </Container>
+            </div>
         );
     }
 }
